fa51449d65acddb007285bcfd99584bab790693f
InfiniteXyy <xuyiyangwing@gmail.com>
2023-12-22 14:32:24 +0800

    first commit

---
+++ src/Message.test.tsx
@@ -0,0 +1,27 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import Message from "./Message";
+import { describe, it, expect, beforeEach } from "vitest";
+
+beforeEach(cleanup);
+
+describe(
+  "test",
+  () => {
+    it("1", () => {
+      const fakeSystemText = `<b>test</b> abc<script>alert("")</script>`;
+      render(<Message text={fakeSystemText} />);
+      const messageElement = screen.getByTestId("message");
+      expect(messageElement).toBeVisible();
+      expect(messageElement.innerHTML).toBe("<b>test</b> abc");
+    });
+
+    it("2", () => {
+      const fakeSystemText = `<b>test</b> abc<script>alert("")</script>`;
+      render(<Message text={fakeSystemText} />);
+      const messageElement = screen.getByTestId("message");
+      expect(messageElement).toBeVisible();
+      expect(messageElement.innerHTML).toBe("<b>test</b> abc");
+    });
+  },
+  { repeats: 200 }
+);
---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+import DOMPurify from 'dompurify';
+
+const dompurify = DOMPurify();
+
+const SystemMessage = ({ text }: { text: string }) => {
+  return (
+    <div
+      // data-testid="message"
+      dangerouslySetInnerHTML={{ __html: dompurify.sanitize(text) }}
+    />
+  );
+};
+
+export default SystemMessage;
